ba8a3eb7ec34d3b34388753de0d84d67ba735ce6
unknown <22yn0116@JN01.JEC.AC.JP>
2023-08-01 20:25:20 +0900

    ユーザ名の変更

--- helpers/config.php
+++ helpers/config.php
@@ -1,5 +1,5 @@
 <?php
 	    // DB接続設定
 	    define('DSN', 'sqlsrv:server = tcp:yn220116.database.windows.net,1433; Database = 22yn0116", "jnsql", "{your_password_here}');
-	    define('DB_USER', 'yn220116');
+	    define('DB_USER', 'jnsql');
 	    define('DB_PASSWORD', 'Pa$$word1234');
